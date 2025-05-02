44589bd3b8f05729755642297ad2476452cb0435
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-05-02 11:00:14 +0530

    pause

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ const App = () => {
 
   return (
     <>
-    <YouTubeAuth/>
       <Navbar clickSideBar={clickSideBar} setClickSideBar={setClickSideBar} />
       <Routes>
         <Route path='/' element={<Home clickSideBar={clickSideBar} />} />
